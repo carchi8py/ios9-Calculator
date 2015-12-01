52f3664062f1d2d3757a7c4cdc3d2366a3af87fb
Chris Archibald <carchi8py@gmail.com>
2015-11-30 19:49:33 -0800

    Number buttons are working.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //All number functions call this function.
+    @IBAction func numericPressed(sender: AnyObject) {
+        var result: Int = Int(resultLabel.text!)!
+        //If the user wants 52 they first push 5.
+        //When they push 2 we move the 5 up a place 50, and then add 2 = 52
+        result = result * 10 + sender.tag!
+        resultLabel.text = "\(result)"
+    }
 
 }
 
